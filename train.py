3a7876a1fa8e673b1c597d504bc350b1aa030425
Amir Pourmand <pourmand1376@gmail.com>
2022-05-15 22:20:38 +0430

    add datetime

--- train.py
+++ train.py
@@ -425,7 +425,9 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                     torch.save(ckpt, best)
                 if (epoch > 0) and (opt.save_period > 0) and (epoch % opt.save_period == 0):
                     torch.save(ckpt, w / f'epoch{epoch}.pt')
-                    os.system('cp -r /content/yolov5/runs /content/drive/MyDrive/KUMC_v5l/')
+                    current_time = datetime.now().isoformat(timespec="minutes")
+                    os.system(f'mkdir -p /content/drive/MyDrive/KUMC_v5l/{current_time}/')
+                    os.system(f'cp -r /content/yolov5/runs /content/drive/MyDrive/KUMC_v5l/{current_time}')
                 del ckpt
                 callbacks.run('on_model_save', last, epoch, final_epoch, best_fitness, fi)
 
