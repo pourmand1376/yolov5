db7c5621a4d54e9edc630ca00e0f15ffba8fef3a
Amir Pourmand <pourmand1376@gmail.com>
2022-07-23 13:17:32 +0430

    fix bug

--- val.py
+++ val.py
@@ -272,9 +272,11 @@ def run(
 
     stats = [torch.cat(x, 0).cpu().numpy() for x in zip(*stats)]  # to numpy
     target_cls = [torch.cat(x, 0).cpu().numpy() for x in zip(*target_cls)] #to_numpy
-    
+    stats.append(target_cls[0])
+    import ipdb
+    ipdb.set_trace()
     if len(stats) and stats[0].any():
-        tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats,target_cls=target_cls[0], plot=plots, save_dir=save_dir, names=names)
+        tp, fp, p, r, f1, ap, ap_class = ap_per_class(*stats, plot=plots, save_dir=save_dir, names=names)
         ap50, ap = ap[:, 0], ap.mean(1)  # AP@0.5, AP@0.5:0.95
         mp, mr, map50, map = p.mean(), r.mean(), ap50.mean(), ap.mean()
         nt = np.bincount(stats[3].astype(int), minlength=nc)  # number of targets per class
