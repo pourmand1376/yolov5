d70fe85a9cab85dc9bad08321010c94c1b9463db
Amir Pourmand <pourmand1376@gmail.com>
2022-08-21 22:52:48 +0430

    add .

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -137,7 +137,7 @@ def create_weighted_sampler(dataset):
 
     final_weights = np.array(final_weights)
     return WeightedRandomSampler(torch.from_numpy(final_weights),
-        num_samples= 30) # this one indicates your iteration size. 
+        num_samples=  int(len(final_weights)/8))  # this one indicates your iteration size. 
 
 
 def create_dataloader(path,
