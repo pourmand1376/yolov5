a31518ddaa4f6f3ba25d5c39bfe07d6119d81472
Amir Pourmand <pourmand1376@gmail.com>
2022-08-21 13:50:22 +0430

    .

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -721,12 +721,12 @@ class LoadImagesAndLabels(Dataset):
         return torch.from_numpy(img), labels_out, self.im_files[index], shapes
 
     def __getitem__(self,index):
-        breakpoint()
-        image,label,file_name,shape= getitem_normal(index)
+        image,label,file_name,shape= self.getitem_normal(index)
 
         def _get_patient(img_index):
             return str.join('_', self.im_files[img_index].split('_')[:-1])
-        
+            
+        breakpoint()
 
     def load_image(self, i):
         # Loads 1 image from dataset index 'i', returns (im, original hw, resized hw)
