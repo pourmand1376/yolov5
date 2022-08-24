d29e0e218dcacc097e87c4a07d8f559da674334a
Amir Pourmand <pourmand1376@gmail.com>
2022-08-24 18:29:45 +0430

    .

--- utils/general.py
+++ utils/general.py
@@ -538,7 +538,7 @@ def check_dataset(data, autodownload=True):
 def check_amp(model):
     # Check PyTorch Automatic Mixed Precision (AMP) functionality. Return True on correct operation
     from models.common import AutoShape, DetectMultiBackend
-
+    breakpoint()
     def amp_allclose(model, im):
         # All close FP32 vs AMP results
         m = AutoShape(model, verbose=True)  # model
